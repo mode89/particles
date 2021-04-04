42ffff01012d0670846adea3169ce7e050b0f562
Andrey Krainyak <mode89@mail.ru>
2021-04-04 17:34:27 +0800

    Build app with reflex-dom
    Going to use reflex for handling events.

--- src/Main.hs
+++ src/Main.hs
@@ -1,90 +1,85 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Control.Lens ((^.))
-import GHCJS.DOM (currentDocumentUnchecked, inAnimationFrame')
+import Data.ByteString (ByteString)
+import qualified Data.Foldable as Foldable
+import Data.Text (Text)
+import GHCJS.DOM (inAnimationFrame')
 import qualified GHCJS.DOM.Element as Element
 import qualified GHCJS.DOM.DOMRect as DOMRect
-import GHCJS.DOM.Document
-    ( createElement
-    , getBodyUnsafe
-    , getDocumentElementUnsafe )
 import qualified GHCJS.DOM.HTMLCanvasElement as Canvas
-import GHCJS.DOM.Node (appendChild)
 import GHCJS.DOM.Types
-    ( ArrayBuffer(..)
-    , Element(..)
-    , GLenum
+    ( GLenum
     , HTMLCanvasElement(..)
     , JSM
-    , JSVal
-    , pToJSVal
+    , liftJSM
     , RenderingContext(..)
     , toJSVal
-    , ToJSVal
     , unsafeCastTo
     , WebGLProgram
     , WebGLRenderingContext(..)
     , WebGLShader )
 import qualified GHCJS.DOM.WebGLRenderingContextBase as GL
-import Language.Javascript.JSaddle (js, jsf, jsg, jss, new)
+import Language.Javascript.JSaddle (jsf, jsg, new)
+import Linear.Matrix (identity, M44)
+import Linear.Projection (ortho)
+import Reflex.Dom (el', _element_raw, mainWidgetWithCss, text)
 
 main :: JSM ()
-main = do
-    document <- currentDocumentUnchecked
+main = mainWidgetWithCss style $ do
+    (canvas, _) <- el' "canvas" $ text ""
+    liftJSM $ do
+        canvasRaw <- unsafeCastTo HTMLCanvasElement $ _element_raw canvas
+        context <- Canvas.getContextUnsafe
+            canvasRaw ("webgl" :: Text) ([] :: [()])
+                >>= unsafeCastTo WebGLRenderingContext
 
-    html <- getDocumentElementUnsafe document
-    styleSet html "display" "flex"
-    styleSet html "flex-direction" "column"
-    styleSet html "height" "100%"
-    styleSet html "margin" "0"
-    body <- getBodyUnsafe document
-    styleSet body "display" "flex"
-    styleSet body "flex-direction" "column"
-    styleSet body "height" "100%"
-    styleSet body "margin" "0"
+        GL.clearColor context 0.5 0.5 0.5 1.0
 
-    canvas <- createElement document "canvas"
-        >>= unsafeCastTo HTMLCanvasElement
-    styleSet canvas "width" "100%"
-    styleSet canvas "height" "100%"
-    appendChild body canvas
-    context <- Canvas.getContextUnsafe canvas "webgl" ([] :: [()])
-        >>= unsafeCastTo WebGLRenderingContext
+        vertexBuffer <- GL.createBuffer context
+        GL.bindBuffer context GL.ARRAY_BUFFER (Just vertexBuffer)
+        toJSVal context ^. jsf ("bufferData" :: Text)
+            ( GL.ARRAY_BUFFER :: GLenum
+            , new (jsg ("Float32Array" :: Text))
+                [[ -1.0, -1.0
+                 , -1.0,  1.0
+                 ,  1.0,  1.0 :: Float ]]
+            , GL.STATIC_DRAW :: GLenum )
 
-    GL.clearColor context 0.5 0.5 0.5 1.0
+        vertexShader <- buildShader context GL.VERTEX_SHADER
+            "attribute vec2 a_position; \
+            \uniform mat4 u_modelViewProjection; \
+            \void main() { \
+            \    gl_Position = \
+            \        vec4(a_position, 0.0, 1.0) * u_modelViewProjection; \
+            \}"
+        fragmentShader <- buildShader context GL.FRAGMENT_SHADER
+            "void main() { \
+            \   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \
+            \}"
+        program <- buildProgram context vertexShader fragmentShader
+        GL.useProgram context (Just program)
 
-    vertexBuffer <- GL.createBuffer context
-    GL.bindBuffer context GL.ARRAY_BUFFER (Just vertexBuffer)
-    toJSVal context ^. jsf "bufferData"
-        ( GL.ARRAY_BUFFER :: GLenum
-        , new (jsg "Float32Array")
-            [[ -1.0, -1.0
-             , -1.0,  1.0
-             ,  1.0,  1.0 :: Float ]]
-        , GL.STATIC_DRAW :: GLenum )
+        positionAttributeLocation <- fromIntegral <$> GL.getAttribLocation
+            context (Just program) ("a_position" :: Text)
+        GL.enableVertexAttribArray context positionAttributeLocation
+        GL.vertexAttribPointer context
+            positionAttributeLocation 2 GL.FLOAT False 0 0
 
-    vertexShader <- buildShader context GL.VERTEX_SHADER
-        "attribute vec2 a_position; \
-        \void main() { \
-        \    gl_Position = vec4(a_position, 0.0, 1.0); \
-        \}"
-    fragmentShader <- buildShader context GL.FRAGMENT_SHADER
-        "void main() { \
-        \   gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); \
-        \}"
-    program <- buildProgram context vertexShader fragmentShader
-    GL.useProgram context (Just program)
+        let matrix = ortho (-2.0) 2.0 (-2.0) 2.0 (-1.0) 1.0 :: M44 Double
+        modelViewProjectionUniform <- GL.getUniformLocation
+            context (Just program) ("u_modelViewProjection" :: Text)
+        GL.uniformMatrix4fv context
+            (Just modelViewProjectionUniform) False $ listFromMatrix matrix
 
-    positionAttributeLocation <- fromIntegral <$>
-        GL.getAttribLocation context (Just program) "a_position"
-    GL.enableVertexAttribArray context positionAttributeLocation
-    GL.vertexAttribPointer context
-        positionAttributeLocation 2 GL.FLOAT False 0 0
+        err <- GL.getError context
+        putStrLn $ "Error: " ++ show err
 
-    err <- GL.getError context
-    putStrLn $ "Error: " ++ show err
+        inAnimationFrame' $ tick context canvasRaw
 
-    inAnimationFrame' $ tick context canvas
+        return ()
 
     return ()
 
@@ -100,10 +95,6 @@ tick context canvas timestamp = do
     GL.drawArrays context GL.TRIANGLES 0 3
     inAnimationFrame' $ tick context canvas
     return ()
-
-styleSet :: (ToJSVal a, ToJSVal b) => a -> String -> b -> JSM ()
-styleSet element property value =
-    toJSVal element ^. js "style" ^. jss property value
 
 buildShader :: WebGLRenderingContext -> GLenum -> String -> JSM WebGLShader
 buildShader context shaderType sourceCode = do
@@ -126,3 +117,19 @@ buildProgram context vertexShader fragmentShader = do
     log <- GL.getProgramInfoLogUnsafe context (Just program)
     putStrLn $ "Program log: " ++ log
     return program
+
+style :: ByteString
+style =
+    "html, body { \
+    \    display: flex; \
+    \    flex-direction: column; \
+    \    height: 100%; \
+    \    margin: 0; \
+    \} \
+    \canvas { \
+    \    width: 100%; \
+    \    height: 100%; \
+    \}"
+
+listFromMatrix :: M44 a -> [a]
+listFromMatrix = concat . fmap Foldable.toList . Foldable.toList
