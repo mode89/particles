d4c16c130d6cb17ec04b87c84beb370d921f611e
Andrey Krainyak <mode89@mail.ru>
2021-04-16 23:57:19 +0800

    Benchmark containers
    Figure out difference in performance between different containers.

--- particles/benchmark/Main.hs
+++ particles/benchmark/Main.hs
@@ -3,7 +3,12 @@
 module Main where
 
 import Criterion.Main
+import Criterion.Types
 import Debug.Trace
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as VM
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector.Unboxed.Mutable as VUM
 import qualified GHCJS.DOM as DOM
 import qualified GHCJS.DOM.Document as Document
 import qualified GHCJS.DOM.HTMLCanvasElement as Canvas
@@ -29,8 +34,47 @@ main = do
     GL.bindBuffer gl GL.ARRAY_BUFFER (Just buffer)
     PGL.bufferDataSizeOnly gl GL.ARRAY_BUFFER 16000000 GL.DYNAMIC_DRAW
     let !bufferData = [0 .. 20000]
+    containers <- prepareContainers
     defaultMain [
           bench "bufferSubDataFloat" $ nfIO $
             PGL.bufferSubDataFloat gl GL.ARRAY_BUFFER 0 bufferData
-        , bench "sum" $ whnf sum bufferData
+        , bgroupContainers containers
         ]
+
+bgroupContainers :: ( [Float]
+                    , V.Vector Float
+                    , VU.Vector Float
+                    , VM.IOVector Float
+                    , VUM.IOVector Float )
+                 -> Benchmark
+bgroupContainers (l, v, vu, vm, vum) = bgroup "containers" [
+      bgroup "sum" [
+          bench "list" $ whnf sum l
+        , bench "vector" $
+            nf (V.foldl (+) 0.0) v
+        , bench "unboxed-vector"
+            $ nf (VU.foldl (+) 0.0) vu
+        ]
+    , bgroup "update" [
+          bench "list" $
+            nf (sum . map (+1.0)) l
+        , bench "vector" $
+            nf (sum . V.map (+1.0)) v
+        , bench "unboxed-vector" $
+            nf (VU.foldl (+) 0.0 . VU.map (+1.0)) vu
+        ]
+    , bgroup "update-inplace" [
+          bench "mutable-vector" $ nfIO $ VM.imapM_
+            (\i x -> VM.write vm i (x + 1.0 :: Float)) vm
+        , bench "mutable-unboxed-vector" $ nfIO $ VUM.imapM_
+            (\i x -> VUM.write vum i (x + 1.0 :: Float)) vum
+        ]
+    ]
+
+prepareContainers = do
+    let l = [0 .. 20000 :: Float]
+    let v = V.fromList [0 .. 20000 :: Float]
+    let vu = VU.fromList [0 .. 20000 :: Float]
+    vm <- VM.generate 20000 fromIntegral
+    vum <- VUM.generate 20000 fromIntegral
+    return (l, v, vu, vm, vum)
