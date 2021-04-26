7ee34f8d7ccedd9363911814d23aa32d851cd210
Andrey Krainyak <mode89@mail.ru>
2021-04-26 23:24:00 +0800

    Update ParticlesMap2
    Map is required for accelerating collision detection.

--- particles/lib/Particles/Map2.hs
+++ particles/lib/Particles/Map2.hs
@@ -4,10 +4,31 @@
 module Particles.Map2 where
 
 import Control.Lens ((^.))
-import Data.IORef (newIORef)
+import Data.IORef (newIORef, readIORef, writeIORef)
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
+import Linear.V2 (_x, _y)
 import Particles.Types
+
+update :: BoundingBox -> Particles2 -> ParticlesMap2 -> IO ParticlesMap2
+update bbox ps pmap = do
+    nextMap <- reset bbox pmap
+    fillBuckets nextMap ps
+    return nextMap
+
+reset :: BoundingBox -> ParticlesMap2 -> IO ParticlesMap2
+reset bbox pmap = do
+    if bbox /= mapBoundingBox pmap
+        then initialize bbox
+        else do
+            resetBuckets $ mapBuckets pmap
+            return pmap
+
+resetBuckets :: MapBuckets -> IO ()
+resetBuckets (MapBuckets buckets) = do
+    V.forM_ buckets $ \MapBucket{..} -> do
+        writeIORef bucketParticlesNum 0
 
 initialize :: BoundingBox -> IO ParticlesMap2
 initialize bbox = do
@@ -27,3 +48,25 @@ newBucket = do
     size <- newIORef 0
     indices <- VUM.new 100
     return $ MapBucket size indices
+
+fillBuckets :: ParticlesMap2 -> Particles2 -> IO ()
+fillBuckets ParticlesMap2{..} ps = do
+    VU.iforM_ ps $ \particleIndex Particle{..} -> do
+        let (MapBuckets buckets) = mapBuckets
+        let MapBucket{..} = V.unsafeIndex buckets $
+                bucketIndex mapBoundingBox mapBucketDim _position
+        bucketParticlesNumValue <- readIORef bucketParticlesNum
+        VUM.write bucketParticlesIndices
+            bucketParticlesNumValue particleIndex
+        writeIORef bucketParticlesNum $ bucketParticlesNumValue + 1
+
+bucketIndex :: BoundingBox -> BucketDim -> Position -> BucketIndex
+bucketIndex BoundingBox{..} bucketDim pos =
+    indexFromRowAndColumn columns row column
+    where
+        column = floor $ (pos ^. _x - _left) / bucketDim
+        row = floor $ (pos ^. _y - _bottom) / bucketDim
+        columns = ceiling $ (_right - _left) / bucketDim
+
+indexFromRowAndColumn :: Int -> Int -> Int -> BucketIndex
+indexFromRowAndColumn width row column = column + row * width
--- particles/tests/Map2.hs
+++ particles/tests/Map2.hs
@@ -2,9 +2,12 @@
 
 module Map2 where
 
+import Control.Monad (forM_)
 import Data.IORef (readIORef)
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as VUM
+import Linear.V2 (V2(..))
 import Particles.Map2
 import Particles.Types
 import Test.Hspec
@@ -19,11 +22,32 @@ map2Spec = describe "Map2" $ do
     it "initialize" $ do
         let bbox = BoundingBox 0 70 0 70
         ParticlesMap2{..} <- initialize bbox
-        let (MapBuckets buckets) = mapBuckets
-        V.length buckets `shouldBe` 4
         mapBoundingBox `shouldBe` bbox
         mapBucketDim `shouldBe` 50
-        V.forM_ buckets $ \MapBucket{..} -> do
-            size <- readIORef $ bucketParticlesNum
-            size `shouldBe` 0
-            VUM.length bucketParticlesIndices `shouldBe` 100
+        buckets <- freezeBuckets mapBuckets
+        buckets `shouldBe` [[], [], [], []]
+    it "update, one particle" $ do
+        let bbox = BoundingBox 0 50 0 50
+        pmap0 <- initialize bbox
+        let particles = VU.fromList [ Particle (V2 25 25) (V2 0 0) ]
+        pmap1 <- update bbox particles pmap0
+        buckets <- freezeBuckets $ mapBuckets pmap1
+        buckets `shouldBe` [[0]]
+    it "update, two particles" $ do
+        let bbox = BoundingBox 0 70 0 70
+        pmap0 <- initialize bbox
+        let particles = VU.fromList
+                [ Particle (V2 60 60) (V2 0 0)
+                , Particle (V2 25 25) (V2 0 0) ]
+        pmap1 <- update bbox particles pmap0
+        buckets <- freezeBuckets $ mapBuckets pmap1
+        buckets `shouldBe` [[1], [], [], [0]]
+
+freezeBuckets :: MapBuckets -> IO [[Int]]
+freezeBuckets (MapBuckets buckets) =
+    V.toList <$> V.mapM freezeBucket buckets
+
+freezeBucket :: MapBucket -> IO [Int]
+freezeBucket MapBucket{..} = do
+    size <- readIORef bucketParticlesNum
+    VUM.foldr (:) [] $ VUM.take size bucketParticlesIndices
